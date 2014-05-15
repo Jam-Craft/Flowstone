30c9d960581a5c768769b755bfd34d22d69f17e3
MrComputerGhost <mr.computerghost@gmail.com>
2014-05-14 19:12:41 -0700

    Commit #5
    MWHAHAHHA

--- src/main/java/net/jamcraft/lasermod/items/ItemLaser.java
+++ src/main/java/net/jamcraft/lasermod/items/ItemLaser.java
@@ -4,6 +4,7 @@ import net.jamcraft.lasermod.TextureConstants;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
@@ -15,10 +16,16 @@ public class ItemLaser extends Item {
 	
 	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player)
     {
-        Vec3 directionLooking = player.getLook(1);
-        Vec3
+        Vec3 dirL = player.getLook(1);
+        Vec3 source = world.getWorldVec3Pool().getVecFromPool(player.posX+dirL.xCoord, player.posZ+dirL.yCoord+player.eyeHeight, player.posZ+dirL.zCoord);
+        Vec3 foo = source.addVector(dirL.xCoord*100, dirL.yCoord*100, dirL.zCoord*100);
+		MovingObjectPosition mop = world.rayTraceBlocks(source, foo, true);
+		if (mop == null) return item;
+		else {
+			world.spawnParticle("smoke", , par4, par6, par8, par10, par12);
+			return item;
+		}
 		
-		return item;
     }
 	
 }
