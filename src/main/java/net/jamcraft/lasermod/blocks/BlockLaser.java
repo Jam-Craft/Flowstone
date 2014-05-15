4f0b32c7c1f2899483d2710523fd108491bf163c
Jackson Isted <dev@hawks008.dyndns.org>
2014-05-15 13:31:49 +1000

    Auto commit on Thu May 15 13:31:49 EST 2014

---
+++ src/main/java/net/jamcraft/lasermod/blocks/BlockLaser.java
@@ -0,0 +1,14 @@
+package net.jamcraft.lasermod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ * @author Jackson Isted
+ * @version 1.0
+ */
+public class BlockLaser extends Block {
+    protected BlockLaser(Material p_i45394_1_) {
+        super(p_i45394_1_);
+    }
+}
