bd47728af1da764ed05c5992c550819428661e81
Jackson Isted <dev@hawks008.dyndns.org>
2014-05-15 13:01:49 +1000

    Auto commit on Thu May 15 13:01:49 EST 2014

---
+++ src/main/java/net/jamcraft/lasermod/entitys/EntityLaser.java
@@ -0,0 +1,31 @@
+package net.jamcraft.lasermod.entitys;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+/**
+ * @author Jackson Isted
+ * @version 1.0
+ */
+public class EntityLaser extends Entity {
+
+    public EntityLaser(World par1World) {
+        super(par1World);
+    }
+
+    @Override
+    protected void entityInit() {
+
+    }
+
+    @Override
+    protected void readEntityFromNBT(NBTTagCompound var1) {
+
+    }
+
+    @Override
+    protected void writeEntityToNBT(NBTTagCompound var1) {
+
+    }
+}
