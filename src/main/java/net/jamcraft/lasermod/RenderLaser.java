08acc7ec6e3ae46c5b84ee8d740d32ddecdf0d37
MrComputerGhost <mr.computerghost@gmail.com>
2014-05-15 15:59:50 -0700

    Commit!

--- src/main/java/net/jamcraft/lasermod/RenderLaser.java
+++ src/main/java/net/jamcraft/lasermod/RenderLaser.java
@@ -11,20 +11,7 @@ import net.minecraft.util.ResourceLocation;
 public class RenderLaser extends Render {
 
     public void doRender(EntityLaser laser, double var2, double var4, double var6, float var8, float var9) {
-	Tessellator tessellator = Tessellator.instance;
-	GL11.glDisable(GL11.GL_TEXTURE_2D);
-	GL11.glDisable(GL11.GL_LIGHTING);
-	for (int k1 = 0; k1 < 4; ++k1)
-	{
-	    tessellator.startDrawing(5);
-	    float f2 = 0.5F;
-	    tessellator.setColorRGBA_F(0.9F * f2, 0.9F * f2, 1.0F * f2, 0.3F);
-	    double d9 = 0.1D + (double) k1 * 0.2D;
-	    tessellator.draw();
-	}
-	GL11.glDisable(GL11.GL_BLEND);
-	GL11.glEnable(GL11.GL_LIGHTING);
-	GL11.glEnable(GL11.GL_TEXTURE_2D);
+	
     }
 
     @Override
