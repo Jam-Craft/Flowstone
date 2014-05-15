c2816e53836ad56af5f1af49958c33f878ecc602
Jackson Isted <dev@hawks008.dyndns.org>
2014-05-15 16:46:52 +1000

    Auto commit on Thu May 15 16:46:52 EST 2014

--- src/main/java/net/jamcraft/lasermod/items/ItemLaser.java
+++ src/main/java/net/jamcraft/lasermod/items/ItemLaser.java
@@ -22,8 +22,8 @@ public class ItemLaser extends Item {
         Vec3 source = world.getWorldVec3Pool().getVecFromPool(player.posX+dirL.xCoord, player.posZ+dirL.yCoord+player.eyeHeight, player.posZ+dirL.zCoord);
         Vec3 foo = source.addVector(dirL.xCoord*100, dirL.yCoord*100, dirL.zCoord*100);
 		MovingObjectPosition mop = world.rayTraceBlocks(source, foo, true);
+        System.out.println(mop);
 		if (mop == null) return item;
-		System.out.println("POOP 2");
         world.setBlock(mop.blockX, mop.blockY, mop.blockZ, Lasers.laserBlock);
 		if (this.getUnlocalizedName().contains("explo")) world.createExplosion(null, mop.blockX, mop.blockY, mop.blockZ, 15, false);
 		else world.spawnParticle("slime", mop.blockX, mop.blockY + 1, mop.blockZ, 0D, 0D, 0D);
