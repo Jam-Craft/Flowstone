1f2ed7ff131928d40a5bb9c2ca877462b349b212
MrComputerGhost <mr.computerghost@gmail.com>
2014-05-17 06:28:16 -0700

    Commit

--- src/main/java/net/jamcraft/flowstone/Flowstone.java
+++ src/main/java/net/jamcraft/flowstone/Flowstone.java
@@ -29,53 +29,16 @@ public class Flowstone {
     public static CommonProxy proxy;
     
     public static Item unstableFlowstone;
-    
-    public static Potion energyFlowtion;
-    public static Potion lightFlowtion;
-    
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
         unstableFlowstone = new ItemFlowstoneMixture(5, 15, true);
-        MinecraftForge.EVENT_BUS.register(new FlowtionHandler());
-        registerFlowtions();
     }
     
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         GameRegistry.registerItem(unstableFlowstone, "unstableFlowstone");
         ChestGenHooks.getInfo("dungeonChest").addItem(new WeightedRandomChestContent(new ItemStack(unstableFlowstone), 4, 16, 75));
-    }
-    
-    public void registerFlowtions() {
-	energyFlowtion = new Flowtion(47, true, 0).setIconIndex(0, 0).setPotionName("flowtion.energy");
-	lightFlowtion = new Flowtion(48, true, 0).setIconIndex(0, 1).setPotionName("flowtion.light");		
-	
-	Potion[] types = null;
-	Field[] fields = Potion.class.getDeclaredFields();
-	int amount = fields.length;
-	for (int currentField = 0; currentField < amount; ++currentField) {
-		Field currentFeild = fields[currentField];
-		currentFeild.setAccessible(true);
-
-		try {
-			if (currentFeild.getName().equals("potionTypes")
-					|| currentFeild.getName().equals("potionTypes")) {
-				Field error = Field.class.getDeclaredField("modifiers");
-				error.setAccessible(true);
-				error.setInt(currentFeild, currentFeild.getModifiers()
-						& -17);
-				types = (Potion[]) ((Potion[]) currentFeild
-						.get((Object) null));
-				Potion[] newPotionTypes = new Potion[256];
-				System.arraycopy(types, 0, newPotionTypes, 0,
-						types.length);
-				currentFeild.set((Object) null, newPotionTypes);
-			}
-		} catch (Exception e) {
-			System.err.println("Error. Please report this to MrComputerGhost!\nCrash Log: \n" + e);
-		}
-	}
     }
     
     
