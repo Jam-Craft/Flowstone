c693074a9c0113c8b043b8330905630ebee7f18f
MrComputerGhost <mr.computerghost@gmail.com>
2014-05-18 16:37:12 -0700

    Commit

--- src/main/java/net/jamcraft/lasermod/ModConstants.java
+++
@@ -1,8 +0,0 @@
-package net.jamcraft.lasermod;
-
-public class ModConstants {
-	
-	public static final String MODID = "lasers";
-	public static final String NAME = "Lasers";
-
-}
--- src/main/java/net/jamcraft/lasermod/TextureConstants.java
+++
@@ -1,5 +0,0 @@
-package net.jamcraft.lasermod;
-
-public class TextureConstants {
-	public static final String RESOURCE_CONTEXT = "lasers";
-}
